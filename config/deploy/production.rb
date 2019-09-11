52d9281bed2e7fdff2c0c5b6271b4f3bec14f9e0
naru20181117 <t.naruhiro.1026@gmail.com>
2019-09-11 17:58:35 +0900

    second time that i put some settings on the local server finallygit add .

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,10 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "3.115.51.128", user: "naruhiro", roles: %w{app db web}
+
+set :ssh_options, keys: '~/.ssh/aws_git_rsa' 
+
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
